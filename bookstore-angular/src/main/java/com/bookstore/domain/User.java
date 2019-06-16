59a4076eb1b69f170196720d6eef8131f0f14289
Vikash singh <vikashrajput519@gmail.com>
2019-06-16 19:21:36 +0530

    admin-angular

---
+++ bookstore-angular/src/main/java/com/bookstore/domain/User.java
@@ -0,0 +1,138 @@
+package com.bookstore.domain;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.bookstore.domain.security.UserRole;
+
+public class User implements UserDetails, Serializable{
+
+	private static final long serialVersionUID = -604192807361353637L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="userId",nullable = false, updatable = false)
+	private Long userId;
+	
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	private String phone;
+	private String email;
+	private boolean enabled = true;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	private Set<UserRole> userRoles = new HashSet<>();
+	
+	
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+}
--- bookstore-angular/src/main/java/com/bookstore/domain/security/UserRole.java
+++ bookstore-angular/src/main/java/com/bookstore/domain/security/UserRole.java
@@ -1,10 +1,28 @@
 package com.bookstore.domain.security;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.bookstore.domain.User;
 
 @Entity
 @Table(name="USERROLE")
 public class UserRole {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String userRoleId;
+	
+	private Role role;
+	private User user;
+	
+	public UserRole(User user, Role role)
+	{
+		this.user = user;
+		this.role = role;
+	}
 
 }
