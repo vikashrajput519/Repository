61c393d595aa2b985b798c58fa1e16fe2a391f36
Vikash singh <vikashrajput519@gmail.com>
2019-06-16 16:07:35 +0530

    Revert "adminportal"
    This reverts commit b7a21d74f0173fc2ef041526afa6ce5626e293e2.

--- store-front/e2e/src/app.e2e-spec.ts
+++ store-front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to bookstore-ui!');
+    expect(page.getTitleText()).toEqual('Welcome to store-front!');
   });
 
   afterEach(async () => {
--- store-front/karma.conf.js
+++ store-front/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/bookstore-ui'),
+      dir: require('path').join(__dirname, './coverage/store-front'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
