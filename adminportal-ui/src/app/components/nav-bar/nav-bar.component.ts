7ea003001c28a53200b143f3a7ef097a2221738b
Vikash singh <vikashrajput519@gmail.com>
2019-06-16 17:09:55 +0530

    a-ui

--- adminportal-ui/src/app/components/nav-bar/nav-bar.component.ts
+++ adminportal-ui/src/app/components/nav-bar/nav-bar.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  private isLoggedIn = false;
 
   constructor() { }
-
+  
+  toggleDisplay()
+  {
+    this.isLoggedIn = !this.isLoggedIn
+  }
   ngOnInit() {
   }
 
