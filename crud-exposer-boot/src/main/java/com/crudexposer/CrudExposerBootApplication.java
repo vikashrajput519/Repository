06d64cdd79b00a360cd97c6963d46ccfb6766d2a
Vikash singh <vikashrajput519@gmail.com>
2019-08-17 09:57:46 +0530

    crud example

---
+++ crud-exposer-boot/src/main/java/com/crudexposer/CrudExposerBootApplication.java
@@ -0,0 +1,13 @@
+package com.crudexposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExposerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExposerBootApplication.class, args);
+	}
+
+}
